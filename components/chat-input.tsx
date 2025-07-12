33b7a344a7efe81227704e7e6d74afdb30392ce4
t-hirata74 <t.hirata.0463@gmail.com>
2025-07-12 18:03:06 +0900

    チャットUIの実装

--- components/chat-input.tsx
+++ components/chat-input.tsx
@@ -1,14 +1,37 @@
-import { ArrowUpIcon } from "./icon";
+"use client";
 
-export function ChatInput() {
+import { ArrowUpIcon } from "./icon";
+import { UseChatHelpers } from "@ai-sdk/react";
+
+export function ChatInput({
+  input,
+  setInput,
+  handleSubmit,
+  status,
+}: {
+  input: UseChatHelpers["input"];
+  setInput: UseChatHelpers["setInput"];
+  handleSubmit: UseChatHelpers["handleSubmit"];
+  status: UseChatHelpers["status"];
+}) {
   return (
-    <form className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl">
+    <form
+      className="flex mx-auto px-4 bg-background pb-4 md:pb-6 gap-2 w-full md:max-w-3xl"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (status === "ready") {
+          handleSubmit();
+        }
+      }}
+    >
       <div className="relative w-full flex flex-col gap-4">
         <textarea
           placeholder="質問してみましょう"
           className="flex w-full border border-input bg-background px-3 py-2 md:text-sm resize-none rounded-2xl bg-muted pb-10 border-zinc-300 dark:border-zinc-700"
           rows={2}
           autoFocus
+          onChange={(e) => setInput(e.target.value)}
+          value={input}
         />
 
         <div className="absolute bottom-0 right-0 p-2 w-fit flex flex-row justify-end">
--- components/chat.tsx
+++ components/chat.tsx
@@ -1,31 +1,21 @@
+"use client";
+
+import { useChat } from "@ai-sdk/react";
+import { ChatInput } from "./chat-input";
 import { Messages } from "./messages";
 
 export function Chat() {
-  const messages = [
-    {
-      id: "1",
-      role: "user",
-      parts: [
-        {
-          type: "text",
-          text: "こんにちは！",
-        },
-      ],
-    },
-    {
-      id: "2",
-      role: "assistant",
-      parts: [
-        {
-          type: "text",
-          text: "こんにちは！何かお手伝いできることはありますか？",
-        },
-      ],
-    },
-  ];
+  const { messages, input, setInput, handleSubmit, status } = useChat();
+
   return (
     <div className="flex flex-col min-w-0 h-dvh bg-background">
       <Messages messages={messages} />
+      <ChatInput
+        input={input}
+        setInput={setInput}
+        handleSubmit={handleSubmit}
+        status={status}
+      />
     </div>
   );
 }
