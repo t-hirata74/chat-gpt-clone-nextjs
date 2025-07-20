4c07d6665a623566354f87c5f09406880110a1ea
t-hirata74 <t.hirata.0463@gmail.com>
2025-07-20 23:47:13 +0900

    touch components/auth-form.tsx

---
+++ components/auth-form.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import Form from "next/form";
+export function AuthForm({
+  action,
+  children,
+}: {
+  action: NonNullable<
+    string | ((formData: FormData) => void | Promise<void>) | undefined
+  >;
+  children: React.ReactNode;
+}) {
+  return (
+    <Form action={action} className="flex flex-col gap-4 px-4 sm:px-16">
+      <div className="flex flex-col gap-2">
+        <input
+          className="flex h-10 w-full rounded-md border border-zinc-200 px-3 py-2 text-md md:text-sm"
+          placeholder="user@acme.com"
+          autoComplete="email"
+          type="email"
+          name="email"
+        />
+      </div>
+      <div className="flex flex-col gap-2">
+        <input
+          className="flex h-10 w-full rounded-md border border-zinc-200 px-3 py-2 text-md md:text-sm"
+          type="password"
+          name="password"
+        />
+      </div>
+      {children}
+    </Form>
+  );
+}
