d0866f2ca710c57146467be78463c8e90b8b2390
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-03 18:25:54 +0900

    サインアップの処理を実装

--- components/signup-form.tsx
+++ components/signup-form.tsx
@@ -1,13 +1,38 @@
 "use client";
 
+import { useRouter } from "next/navigation";
 import { AuthForm } from "./auth-form";
+import { useActionState, useEffect } from "react";
+import { Signup, SignupActionState } from "@/app/(auth)/actions";
 
 export function SignupForm() {
+  const router = useRouter();
+
+  const [state, formAction] = useActionState<SignupActionState, FormData>(
+    Signup,
+    {
+      status: "idle",
+    },
+  );
+
+  useEffect(() => {
+    if (state.status === "success") {
+      router.push("/");
+    } else if (state.status === "idle") {
+      console.log("idle");
+    } else {
+      alert(state.status);
+    }
+  }, [state]);
+
+  const handleSubmit = (formData: FormData) => {
+    formAction(formData);
+  };
+
   return (
-    // <AuthForm>
-    <AuthForm action="/api/auth/signup">
+    <AuthForm action={handleSubmit}>
       <button
-        className="inline-flex items-center justify-center gap-2 border border-zinc-200 rounded-md text-sm cursor-pointer font-medium dark:hover:bg-zinc-800 hover:bg-zinc-200 h-10 px-4 py-2"
+        className="inline-flex items-center justify-center gap-2 border border-zinc-200 rounded-md text-sm cursor-pointer font-medium hover:bg-zinc-200 h-10 px-4 py-2"
         type="submit"
       >
         アカウント登録
