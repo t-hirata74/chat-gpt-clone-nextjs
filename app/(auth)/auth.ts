0ffd7b41a950b4e0be2ff1e46040ce3994a718eb
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-03 17:56:58 +0900

    authorize関数を実装

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -1,3 +1,5 @@
+import { getUserByEmail } from "@/lib/db";
+import { compareSync } from "bcrypt-ts";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 
@@ -5,8 +7,23 @@ export const { handlers, signIn } = NextAuth({
   providers: [
     Credentials({
       credentials: {},
-      async authorize(credentials) {
-        return null;
+      async authorize({ email, password }: any) {
+        const user = await getUserByEmail(email);
+
+        if (!user) {
+          throw new Error("user not found");
+        }
+
+        if (!user.passwordHash) {
+          throw new Error("user password not exists");
+        }
+
+        const result = compareSync(password, user.passwordHash);
+        if (!result) {
+          throw new Error("user password not match");
+        }
+
+        return user;
       },
     }),
   ],
