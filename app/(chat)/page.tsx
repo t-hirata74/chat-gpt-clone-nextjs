42542af3e4c9ca48d308a1f1bcdd60ffda54bd4b
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-03 20:49:32 +0900

    チャットページを認証する

--- app/(chat)/page.tsx
+++ app/(chat)/page.tsx
@@ -1,5 +1,12 @@
 import { Chat } from "@/components/chat";
+import { auth } from "@/app/(auth)/auth";
+import { redirect } from "next/navigation";
 
-export default function Page() {
+export default async function Page() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/signup");
+  }
   return <Chat />;
 }
