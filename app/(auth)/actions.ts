b7256aa66e33815d911f7b8066a1a904d984a016
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-03 17:51:28 +0900

    server actionを追加

---
+++ app/(auth)/actions.ts
@@ -0,0 +1,49 @@
+"use server";
+
+import { signIn } from "./auth";
+import * as v from "valibot";
+import { createUser, getUserByEmail } from "@/lib/db";
+import { genSaltSync, hashSync } from "bcrypt-ts";
+
+export interface SignupActionState {
+  status: "idle" | "success" | "failed" | "user_exists" | "invalid_data";
+}
+
+export const Signup = async (
+  _: SignupActionState,
+  formData: FormData,
+): Promise<SignupActionState> => {
+  try {
+    const validatedData = await v.parseAsync(
+      v.object({
+        email: v.pipe(v.string(), v.email()),
+        password: v.pipe(v.string(), v.minLength(8), v.maxLength(20)),
+      }),
+      Object.fromEntries(formData.entries()),
+    );
+
+    const user = await getUserByEmail(validatedData.email);
+
+    if (user) {
+      return { status: "user_exists" } as SignupActionState;
+    }
+    const salt = genSaltSync(10);
+    const passwordHash = hashSync(validatedData.password, salt);
+    await createUser(validatedData.email, passwordHash);
+
+    await signIn("credentials", {
+      email: validatedData.email,
+      password: validatedData.password,
+      redirect: false,
+    });
+
+    return { status: "success" };
+  } catch (error) {
+    if (error instanceof v.isValiError) {
+      return { status: "invalid_data" };
+    }
+
+    console.error(error);
+    return { status: "failed" };
+  }
+};
