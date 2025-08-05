e624258f02d6c060810639a0ea1cf9f90cb71ca4
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-05 21:48:44 +0900

    サインイン処理を実装

--- app/(auth)/actions.ts
+++ app/(auth)/actions.ts
@@ -9,6 +9,10 @@ export interface SignupActionState {
   status: "idle" | "success" | "failed" | "user_exists" | "invalid_data";
 }
 
+export interface SigninActionState {
+  status: "idle" | "success" | "failed" | "invalid_data";
+}
+
 export const Signup = async (
   _: SignupActionState,
   formData: FormData,
@@ -44,6 +48,38 @@ export const Signup = async (
     }
 
     console.error(error);
+    return { status: "failed" };
+  }
+};
+
+export const signin = async (
+  _: SigninActionState,
+  formData: FormData,
+): Promise<SigninActionState> => {
+  try {
+    const validatedData = await v.parseAsync(
+      v.object({
+        email: v.pipe(v.string(), v.email()),
+        password: v.pipe(v.string(), v.minLength(8), v.maxLength(20)),
+      }),
+      Object.fromEntries(formData.entries()),
+    );
+
+    const user = await getUserByEmail(validatedData.email);
+    if (!user) {
+      return { status: "invalid_data" } as SigninActionState;
+    }
+    await signIn("credentials", {
+      email: validatedData.email,
+      password: validatedData.password,
+      redirect: false,
+    });
+
+    return { status: "success" };
+  } catch (error) {
+    if (error instanceof v.isValiError) {
+      return { status: "invalid_data" };
+    }
     return { status: "failed" };
   }
 };
