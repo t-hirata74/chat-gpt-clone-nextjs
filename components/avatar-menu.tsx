ebcec5e91f8e1fa26bf8b0512a923124dd48baa1
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-07 23:26:30 +0900

    ログアウトリンクを追加

--- components/avatar-menu.tsx
+++ components/avatar-menu.tsx
@@ -1,3 +1,4 @@
+import { signOut } from "next-auth/react";
 import { AvatarIcon } from "./icon";
 
 export function AvatarMenu() {
@@ -14,7 +15,14 @@ export function AvatarMenu() {
       </div>
       <div className="absolute right-0 z-100 mt-2 w-56 origin-top-right rounded-md shadow-lg dark:bg-zinc-800 ring-black/5">
         <div className="py-1" role="none">
-          <button className="block px-4 py-2 text-sm">ログアウト</button>
+          <button
+            className="block px-4 py-2 text-sm"
+            onClick={() => {
+              signOut({ redirectTo: "/" });
+            }}
+          >
+            ログアウト
+          </button>
         </div>
       </div>
     </div>
