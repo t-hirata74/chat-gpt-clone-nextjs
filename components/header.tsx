16549999411f1d45ac593f81831132ecf91ac36b
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-07 22:41:33 +0900

    ヘッダーUIの実装

---
+++ components/header.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { AvatarIcon } from "./icon";
+
+export function ChatHeader() {
+  return (
+    <header className="flex sticky top-0 bg-background py-1.5 justify-between items-center px-2 md:px-2 gap-2 h-16">
+      <div className="flex"></div>
+      <AvatarIcon />
+    </header>
+  );
+}
