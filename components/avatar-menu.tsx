b80d1e33c8e4e903d1d4705b1601b3be4bae47c2
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-07 23:45:04 +0900

    ドロップダウンを動作させる

--- components/avatar-menu.tsx
+++ components/avatar-menu.tsx
@@ -1,7 +1,9 @@
 import { signOut } from "next-auth/react";
+import { useState } from "react";
 import { AvatarIcon } from "./icon";
 
 export function AvatarMenu() {
+  const [open, setOpen] = useState(false);
   return (
     <div className="relative inline-block text-left">
       <div>
@@ -9,22 +11,27 @@ export function AvatarMenu() {
           type="button"
           className="inline-flex w-full justify-center gap-x-1.5 rounded-md px-3 py-2 text-sm"
           id="menu-button"
+          onClick={() => {
+            setOpen((prev) => !prev);
+          }}
         >
           <AvatarIcon />
         </button>
       </div>
-      <div className="absolute right-0 z-100 mt-2 w-56 origin-top-right rounded-md shadow-lg dark:bg-zinc-800 ring-black/5">
-        <div className="py-1" role="none">
-          <button
-            className="block px-4 py-2 text-sm"
-            onClick={() => {
-              signOut({ redirectTo: "/" });
-            }}
-          >
-            ログアウト
-          </button>
+      {open && (
+        <div className="absolute right-0 z-100 mt-2 w-56 origin-top-right rounded-md shadow-lg dark:bg-zinc-800 ring-black/5">
+          <div className="py-1" role="none">
+            <button
+              className="block px-4 py-2 text-sm"
+              onClick={() => {
+                signOut({ redirectTo: "/" });
+              }}
+            >
+              ログアウト
+            </button>
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
