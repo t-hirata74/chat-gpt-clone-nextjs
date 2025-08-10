58b1284cf5ebb0d8ebf202328e3f0441e48019db
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-10 19:15:57 +0900

    チャットで認証を行う

--- app/(chat)/api/chat/route.ts
+++ app/(chat)/api/chat/route.ts
@@ -1,10 +1,17 @@
+import { auth } from "@/app/(auth)/auth";
 import { google } from "@ai-sdk/google";
-import { streamText } from "ai";
+import { streamText, UIMessage } from "ai";
 
 export const maxDuration = 30;
 
 export async function POST(req: Request) {
-  const { messages } = await req.json();
+  const { id, messages }: { id: string; messages: Array<UIMessage> } =
+    await req.json();
+
+  const session = await auth();
+  if (!session?.user?.id) {
+    return new Response("Unauthorized", { status: 401 });
+  }
 
   const result = streamText({
     model: google("gemini-2.0-flash"),
