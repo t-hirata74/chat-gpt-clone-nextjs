89eebfbb50c81f08e356a36ba229655492a79467
t-hirata74 <t.hirata.0463@gmail.com>
2025-08-19 22:50:29 +0900

    session.user.idを設定する

--- app/(auth)/auth.ts
+++ app/(auth)/auth.ts
@@ -27,4 +27,19 @@ export const { handlers, signIn, auth } = NextAuth({
       },
     }),
   ],
+  callbacks: {
+    async jwt({ token, user, session }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
 });
